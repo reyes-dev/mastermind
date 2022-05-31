70334c2c781778aa46c377f579afbc23f43907e1
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-05-30 20:50:45 -0700

    Update comments and code

--- script.rb
+++ script.rb
@@ -1,8 +1,9 @@
 module CodeGenerator
+  # create a random secret code to solve
   def generate(array)
     array.each_index { |idx| array[idx] = rand(1..6) }
   end
-
+  # lets the cpu guess a new number every round
   def modify_guess(array, enemy_array)
     array.each_index do |idx|
       unless array[idx] == enemy_array[idx]
@@ -13,6 +14,7 @@ module CodeGenerator
 end
 
 module Checkable
+  # check to see if your guess matches both position and number
   def match_type_position
     @human_array.each_index do |idx|
       if @human_array[idx] == @comp_array[idx]
@@ -22,7 +24,7 @@ module Checkable
       end
     end
   end
-
+  # check to see if your guess only matches a number
   def match_type
     @human_array.each_with_index do |num, idx|
       if @comp_array.any?(num)
@@ -32,7 +34,7 @@ module Checkable
       end
     end
   end
-
+  # check if both codes match completely
   def match_full
     if @human_code == @comp_code
       @game_over = true
@@ -48,11 +50,11 @@ end
 
 class Board
   attr_accessor :board_array, :board
-
+  # creates a display board of array type
   def initialize
     @board_array = Array.new(12) { Array.new(4, '?') }
   end
-
+  # prepares the board to be displayed, converted to string with hints
   def board_setup
     @board = ''
 
@@ -61,7 +63,7 @@ class Board
       @board.concat("   --------------\n") 
     end
   end
-
+  # displays the board
   def board_display
     self.board_setup
     puts @board
@@ -83,17 +85,17 @@ class GameLogic < Board
     @comp_array = []
     @human_array = []
   end
-
+  # adds the codes to array in order to check equivalence every round
   def equalize_codes
     @comp_array.concat(@comp_code)
     @human_array.concat(@human_code)
   end
-
+  # clears out codes before the round ends so they can be concatenated anew
   def clear_codes
     @comp_array.clear
     @human_array.clear
   end
-
+  # Takes input only of a 4-digit code of digits between numbers 1-6
   def input_code
     loop do
       puts "Enter a 4-digit code of numbers 1-6"
@@ -101,7 +103,7 @@ class GameLogic < Board
       break if @human_code.join.match?(/^[1-6]{4}$/)
     end
   end
-
+  # updates the board display each round with the guessed code for that turn
   def update_board(guesser_code)
     @board_array[@round].replace(guesser_code)
   end
@@ -111,7 +113,7 @@ class Game < GameLogic
   def initialize
     super
   end
-
+  # calls methods to have the human player guess the code
   def play_human
     puts "AI is Mastermind"
     until game_over || @round > 11
@@ -126,7 +128,7 @@ class Game < GameLogic
     self.board_display
     puts "\n The secret code was: #{self.comp_code} \n"
   end
-
+  # calls methods to have the human player create the code that the cpu then guesses
   def play_computer
     puts "You are the Mastermind"
     self.input_code
@@ -142,12 +144,18 @@ class Game < GameLogic
     self.board_display
     puts "\n The final AI guess was: #{self.comp_code} \n"
   end
-
+  # loop to choose who the mastermind will be
   def choose_mastermind
     loop do
       puts "Enter 1 to be the Mastermind | Enter 2 for AI Mastermind"
       @mm_choice = gets.chomp.split('').map(&:to_i)
-      @mm_choice == [2] ? self.play_human : @mm_choice == [1] ? self.play_computer : "Wrong"
+      if @mm_choice == [2] 
+        self.play_human 
+      elsif @mm_choice == [1] 
+        self.play_computer 
+      else 
+        "Wrong"
+      end
       break if @mm_choice.join.match?(/^[1-2]{1}$/)
     end
   end
